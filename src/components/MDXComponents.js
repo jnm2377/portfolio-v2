02fccf36a0e9bf37e4e58450982722ead9c421fe
Josefina Mancilla <josefinanoemi4@gmail.com>
2022-12-07 22:54:18 -0600

    chore: delete unused MDXComponents export

--- src/components/MDXComponents.js
+++
@@ -1,8 +0,0 @@
-import { PostBackClick } from './PostBackClick';
-// import styles from '../styles/Markdown.module.css';
-
-const MDXComponents = {
-  PostBackClick,
-};
-
-export default MDXComponents;
