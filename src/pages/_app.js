fa74058fc76918f942a816e0ea62fccb1b96d85d
Josefina Mancilla <josefinanoemi4@gmail.com>
2022-12-07 22:54:51 -0600

    chore: delete MDXComponents app import

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,11 +2,10 @@ import '../scss/index.scss';
 import '../scss/app.scss';
 import { ThemePreference } from '../components/ThemePreference';
 import { MDXProvider } from '@mdx-js/react';
-import MDXComponents from '../components/MDXComponents';
 
 export default function App({ Component, pageProps }) {
   return (
-    <MDXProvider components={{ ...MDXComponents }}>
+    <MDXProvider>
       <ThemePreference>
         <Component {...pageProps} />
       </ThemePreference>
