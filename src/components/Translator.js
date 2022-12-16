33aab055fb4eddde50abac97f1c11f0ed3d92662
Josefina Mancilla <josefinanoemi4@gmail.com>
2022-12-16 01:35:32 -0600

    feat: add translation support thru labels vs child

--- src/components/Translator.js
+++ src/components/Translator.js
@@ -1,13 +1,18 @@
 import { useLanguagePreference } from './LanguagePreference';
 
-const Translator = ({ children }) => {
+const Translator = ({ children, englishLabel, spanishLabel }) => {
   const { spanish } = useLanguagePreference();
 
   const hasTranslation = Array.isArray(children);
   const spanishItem = spanish ? 1 : 0;
 
-  // eslint-disable-next-line react/jsx-no-useless-fragment
-  return <>{hasTranslation ? children[spanishItem] : children}</>;
+  if (children) {
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    return <>{hasTranslation ? children[spanishItem] : children}</>;
+  } else {
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    return <>{!spanish ? englishLabel : spanishLabel}</>;
+  }
 };
 
 export default Translator;
