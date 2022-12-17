126e426de89c0af2a3e10574b7bc2e0c3811d5ff
Josefina Mancilla <josefinanoemi4@gmail.com>
2022-12-16 22:01:49 -0600

    feat: add h3 styles

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -12,12 +12,40 @@ import rehypeImgSize from 'rehype-img-size';
 import Translator from '../../components/Translator';
 import English from '../../components/markdown/English';
 import Spanish from '../../components/markdown/Spanish';
+import {
+  Grid,
+  Column,
+  Link,
+  OrderedList,
+  UnorderedList,
+  ListItem,
+} from '@carbon/react';
 
 const components = {
   img: Image,
   Translator,
   English,
   Spanish,
+  ul: UnorderedList,
+  ol: OrderedList,
+  a: Link,
+  Grid,
+  Column,
+  li: ListItem,
+  h3: ({ children }) => {
+    return (
+      <h3
+        style={{
+          fontWeight: '600',
+          marginTop: '2rem',
+          marginBottom: '-1.5rem',
+          fontSize: '1.25rem',
+        }}
+      >
+        {children}
+      </h3>
+    );
+  },
 };
 
 export default function PostPage({ source, frontMatter }) {
